fbc55261ecc967a3cafa8956c6deeecc98e9d82b
solsarr <solomonsarr18@gmail.com>
2022-12-09 09:56:11 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Data from './components/data';
+import Data from './components/Data';
 import { useEffect, useState } from "react";
 import Base from './components/base';
 import DateTime from './components/time';
